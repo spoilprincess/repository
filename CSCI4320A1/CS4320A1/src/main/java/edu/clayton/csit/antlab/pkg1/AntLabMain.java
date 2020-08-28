05a7a2c89303d66e1e770839863a11c46a8a34e7
rsoberanes <69826736+rsoberanes@users.noreply.github.com>
2020-08-28 15:13:26 -0400

    Update AntLabMain.java

--- CSCI4320A1/CS4320A1/src/main/java/edu/clayton/csit/antlab/pkg1/AntLabMain.java
+++ CSCI4320A1/CS4320A1/src/main/java/edu/clayton/csit/antlab/pkg1/AntLabMain.java
@@ -73,8 +73,8 @@ import edu.clayton.csit.antlab.pkg2.*;
                   toPrint += p4.toString("gburdell4");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
+                  Person5 p5 = new Person5("Raziel");
+                  toPrint += p5.toString("900500210");
 		  
         System.out.println(toPrint);
         
